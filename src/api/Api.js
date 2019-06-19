116c9bdad025cfd05c7f9aea7ac493604d02a9be
Matheus Allein <mtsallein@gmail.com>
2019-06-19 10:19:12 -0300

    Setting project up

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,9 +1,19 @@
 import React from 'react';
+// import axios from 'axios';
 
 function Api() {
+
+  // axios.get('/jobs')
+  //   .then((res) => res.json())
+  //   .catch((err) => console.error(err))
+  //   .finally((data) => {
+  //     // Handle retrieved data:
+
+  //   });
+
   return(
-    'helo'
-  )
+    <p>Home</p>
+  );
 }
 
 export default Api;
