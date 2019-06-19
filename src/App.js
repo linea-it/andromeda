dabce4690c33685eaa248ba406733d29f85086b2
Matheus Allein <mtsallein@gmail.com>
2019-06-19 10:31:57 -0300

    Setting project up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Home from './Home';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import theme from './theme/MaterialTheme';
 
 function App() {
   return (
-    <MuiThemeProvider theme={theme}>
+    <MuiThemeProvider>
       <Home />
     </MuiThemeProvider>
   );
