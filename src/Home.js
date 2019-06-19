204a22c43383645d13150e98f889ebf6d959d536
Matheus Allein <mtsallein@gmail.com>
2019-06-19 09:45:27 -0300

    Setting project up

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,15 @@
 import React from 'react';
+// import Api from './api/Api';
 // import PropTypes from 'prop-types';
 
 
 function Home() {
   return (
-    <h1>Hello, World!</h1>
+    <div>
+      <h1>Hello, World!</h1>
+      <Api />
+
+    </div>
   );
 }
 
