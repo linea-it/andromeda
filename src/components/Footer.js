10b00b704a90045aa17e2aeb96415de64dd1af6f
Matheus Freitas <mtsallein@gmail.com>
2019-07-18 17:24:21 -0300

    Adopting the same footer as the portals (#20)

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,28 +1,120 @@
-import * as React from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import { Typography, Toolbar } from '@material-ui/core';
+import logo from '../assets/img/linea-logo-mini.png';
+// eslint-disable-next-line camelcase
+import git_json from '../assets/json/version.json';
 
-const useStyles = makeStyles(({
-  footerWrapper: {
-    float: 'left',
-    width: '100%',
-    height: 41,
-    boxShadow: '0px -2px 4px -1px rgba(0, 0, 0, 0.2)',
-  },
-  footerText: {
-    textAlign: 'center',
-    fontSize: 13,
-    color: '#858797',
-  },
-}));
+function Footer(props) {
+  const { drawerOpen } = props;
+  const useStyles = makeStyles(theme => ({
+    root: {
+      width: '100%',
+      float: 'right',
+      height: 64,
+    },
+    grow: {
+      flexGrow: 1,
+    },
+    appBarDrawerOpen: {
+      top: 'auto',
+      bottom: 0,
+      backgroundColor: '#596C7B',
+      width: 'calc(100% - 240px)',
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.enteringScreen,
+      }),
+    },
+    appBarDrawerClose: {
+      top: 'auto',
+      bottom: 0,
+      backgroundColor: '#596C7B',
+      width: `calc(100% - ${theme.spacing(7) - 1}px)`,
+      transition: theme.transitions.create('width', {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+    },
+    toolbar: {
+      alignItems: 'center',
+      justifyContent: 'space-between',
+    },
+    versionLink: {
+      color: '#d2cf00',
+      textDecoration: 'none',
+      fontSize: '0.9rem',
+      cursor: 'pointer',
+    },
+    logoLink: {
+      lineHeight: 0,
+    },
+  }));
 
-function Footer() {
   const classes = useStyles();
 
+  const openGithub = (vlink) => {
+    if (vlink) {
+      window.open(vlink);
+    }
+  };
+
+  const instance = process.env.NODE_ENV === 'production'
+    // eslint-disable-next-line no-underscore-dangle
+    ? window._env_.REACT_APP_INSTANCE
+    : process.env.REACT_APP_INSTANCE;
+
+  let version = '--';
+  let vlink = null;
+
+  if (
+    Object.entries(git_json).length !== 0
+    && git_json.constructor === Object
+  ) {
+    if (git_json.tag) {
+      version = `${git_json.tag}`;
+      vlink = `${git_json.url}`;
+    } else {
+      version = `${git_json.sha.substring(0, 7)}`;
+      vlink = `${git_json.url.replace(/.git$/, '')}/commit/${git_json.sha}`;
+    }
+  }
+
   return (
-    <footer className={classes.footerWrapper}>
-      <p className={classes.footerText}>Portal Monitor</p>
+    <footer className={classes.root}>
+      <AppBar position="fixed" className={drawerOpen ? classes.appBarDrawerOpen : classes.appBarDrawerClose}>
+        <Toolbar className={classes.toolbar}>
+          <Typography color="inherit" className={classes.grow}>
+            {instance}
+            {' '}
+            Portal:
+            {' '}
+            <span
+              onClick={() => openGithub(vlink)}
+              className={classes.versionLink}
+            >
+              {version}
+            </span>
+          </Typography>
+          <Typography color="inherit">Powered by</Typography>
+          <a href="http://www.linea.gov.br/" target="blank" className={classes.logoLink}>
+            <img
+              src={logo}
+              title="LIneA"
+              alt="LineA"
+              style={{ cursor: 'pointer', marginLeft: '10px' }}
+            />
+          </a>
+        </Toolbar>
+      </AppBar>
     </footer>
   );
 }
+
+Footer.propTypes = {
+  drawerOpen: PropTypes.bool.isRequired,
+};
 
 export default Footer;
