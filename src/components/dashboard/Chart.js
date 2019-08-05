5dadbc617be04e8059106250d67da918177b44dd
Matheus Allein <mtsallein@gmail.com>
2019-08-05 11:09:56 -0300

    Updating plots and logic in Dashboard

--- src/components/dashboard/Chart.js
+++ src/components/dashboard/Chart.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
+import Grid from '@material-ui/core/Grid';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import { Typography } from '@material-ui/core';
@@ -32,16 +33,33 @@ const useStyles = makeStyles(({
     float: 'left',
     textAlign: 'center',
   },
+  root: {
+    flexGrow: 1,
+  },
+  cardsContainer: {
+    paddingTop: 20,
+  },
+  imgResponsive: {
+    maxWidth: '100%',
+    width: '100%',
+    height: 'auto',
+  },
 }));
 
 export default function Chart() {
   const classes = useStyles();
   const [count, setCount] = useState(1);
-  const [images, setImages] = useState([
-    'https://srvlupa.linea.gov.br/gangliaicx/graph.php?r=hour&z=xlarge&me=ICE&m=load_one&s=by+hosts+up&mc=2&g=mem_report&st&st=0',
-    'https://srvlupa.linea.gov.br/gangliaicx/graph.php?r=hour&z=xlarge&me=ICE&m=load_one&s=by+name&mc=2&g=cpu_report&st==0',
-    'https://srvlupa.linea.gov.br/gangliaicx/graph.php?r=hour&z=xlarge&me=ICE&m=load_one&s=by+hosts+up&mc=2&g=network_report&st=0',
-    'https://srvlupa.linea.gov.br/gangliaicx/stacked.php?m=load_one&c=Compute%20Nodes&r=hour&st==0',
+  const [icex, setIcex] = useState([
+    'http://condorapi.linea.gov.br/gangliaicx/graph.php?c=Compute+Nodes&m=load_one&r=hour&s=by+name&hc=4&mc=2&st=1563362232&g=mem_report&z=medium&_=1563364904424&st=',
+    'http://condorapi.linea.gov.br/gangliaicx/graph.php?c=Compute+Nodes&m=load_one&r=hour&s=by+name&hc=4&mc=2&st=1563362232&g=cpu_report&z=medium&_=1563364904425&st=',
+    'http://condorapi.linea.gov.br/gangliaicx/graph.php?c=Compute+Nodes&m=load_one&r=hour&s=by+name&hc=4&mc=2&st=1563362232&g=network_report&z=medium&_=1563364904425&st=',
+    'http://condorapi.linea.gov.br/gangliaicx/stacked.php?m=load_one&c=Compute+Nodes&r=hour&st=1563364897&host_regex=&st=',
+  ]);
+  const [altix, setAltix] = useState([
+    'http://condorapi.linea.gov.br/gangliaaltix/graph.php?c=Rocks-Cluster%20Production%20LIneA&m=load_one&r=hour&s=by%20name&hc=4&mc=2&g=mem_report&z=medium&st=',
+    'http://condorapi.linea.gov.br/gangliaaltix/graph.php?c=Rocks-Cluster%20Production%20LIneA&m=load_one&r=hour&s=by%20name&hc=4&mc=2&g=cpu_report&z=medium&st=',
+    'http://condorapi.linea.gov.br/gangliaaltix/graph.php?c=Rocks-Cluster%20Production%20LIneA&m=load_one&r=hour&s=by%20name&hc=4&mc=2&g=network_report&z=medium&st=',
+    'http://condorapi.linea.gov.br/gangliaaltix/stacked.php?m=load_one&c=Rocks-Cluster%20Production%20LIneA&r=hour&st=',
   ]);
 
   useInterval(() => {
@@ -49,80 +67,135 @@ export default function Chart() {
   }, 2000);
 
   useEffect(() => {
-    setImages([
-      `https://srvlupa.linea.gov.br/gangliaicx/graph.php?r=hour&z=xlarge&me=ICE&m=load_one&s=by+hosts+up&mc=2&g=mem_report&st&st=${count}`,
-      `https://srvlupa.linea.gov.br/gangliaicx/graph.php?r=hour&z=xlarge&me=ICE&m=load_one&s=by+name&mc=2&g=cpu_report&st==${count}`,
-      `https://srvlupa.linea.gov.br/gangliaicx/graph.php?r=hour&z=xlarge&me=ICE&m=load_one&s=by+hosts+up&mc=2&g=network_report&st=${count}`,
-      `https://srvlupa.linea.gov.br/gangliaicx/stacked.php?m=load_one&c=Compute%20Nodes&r=hour&st==${count}`,
-    ]);
+    setIcex(
+      icex.map(node => node + count),
+    );
+    setAltix(
+      altix.map(node => node + count),
+    );
   }, [count]);
 
 
   return (
     <React.Fragment>
-      <div style={{
-        display: 'flex',
-        flexWrap: 'wrap',
-        justifyContent: 'space-around',
-        overflow: 'hidden',
-      }}
-      >
-        <GridList
-          cellHeight={320}
-          style={{
-            width: '100%',
-            height: 330,
-          }}
-          cols={3}
-        >
-          <GridListTile>
-            <Typography
-              variant="h6"
-              className={classes.plotTitle}
+      <div className={classes.root}>
+        <Grid container spacing={3} className={classes.cardsContainer}>
+          <Grid item xs={12}>
+            <div style={{
+              display: 'flex',
+              flexWrap: 'wrap',
+              justifyContent: 'space-around',
+              overflow: 'hidden',
+            }}
             >
-              Memory Usage
-            </Typography>
-            <img src={images[0]} alt="Ganglia chart" />
-          </GridListTile>
-          <GridListTile>
-            <Typography
-              variant="h6"
-              className={classes.plotTitle}
-            >
-              CPU Usage
-            </Typography>
-            <img src={images[1]} alt="Ganglia chart" />
-          </GridListTile>
-          <GridListTile>
-            <Typography
-              variant="h6"
-              className={classes.plotTitle}
-            >
-              Network Usage
-            </Typography>
-            <img src={images[2]} alt="Ganglia chart" />
-          </GridListTile>
-        </GridList>
-        <GridList
-          cellHeight={470}
-          style={{
-            margin: '10px auto 0',
-            height: 500,
-            width: '100%',
-          }}
-          cols={1}
-        >
-          <GridListTile style={{ textAlign: 'center' }}>
-            <Typography
-              variant="h6"
-              className={classes.plotTitle}
-            >
-              Aggregated Cluster Load
-            </Typography>
-            <img src={images[3]} alt="Ganglia chart" style={{ maxWidth: 680, margin: 'auto', textAlign: 'center' }} />
-          </GridListTile>
-        </GridList>
+              <GridList
+                cellHeight="auto"
+                cols={3}
+              >
+                <GridListTile>
+                  <Typography
+                    variant="h6"
+                    className={classes.plotTitle}
+                  >
+                    <strong>[ICEx]</strong>
+                    {' '}
+                    Memory Usage
+                  </Typography>
+                  <img src={icex[0]} alt="Ganglia chart" className={classes.imgResponsive} />
+                </GridListTile>
+                <GridListTile>
+                  <Typography
+                    variant="h6"
+                    className={classes.plotTitle}
+                  >
+                    <strong>[ICEx]</strong>
+                    {' '}
+                  CPU Usage
+                  </Typography>
+                  <img src={icex[1]} alt="Ganglia chart" className={classes.imgResponsive} />
+                </GridListTile>
+                <GridListTile>
+                  <Typography
+                    variant="h6"
+                    className={classes.plotTitle}
+                  >
+                    <strong>[ICEx]</strong>
+                    {' '}
+                  Network Usage
+                  </Typography>
+                  <img src={icex[2]} alt="Ganglia chart" className={classes.imgResponsive} />
+                </GridListTile>
+              </GridList>
+              <GridList
+                cellHeight="auto"
+                cols={3}
+              >
+                <GridListTile>
+                  <Typography
+                    variant="h6"
+                    className={classes.plotTitle}
+                  >
+                    <strong>[Altix]</strong>
+                    {' '}
+                  Memory Usage
+                  </Typography>
+                  <img src={altix[0]} alt="Ganglia chart" className={classes.imgResponsive} />
+                </GridListTile>
+                <GridListTile>
+                  <Typography
+                    variant="h6"
+                    className={classes.plotTitle}
+                  >
+                    <strong>[Altix]</strong>
+                    {' '}
+                  CPU Usage
+                  </Typography>
+                  <img src={altix[1]} alt="Ganglia chart" className={classes.imgResponsive} />
+                </GridListTile>
+                <GridListTile>
+                  <Typography
+                    variant="h6"
+                    className={classes.plotTitle}
+                  >
+                    <strong>[Altix]</strong>
+                    {' '}
+                  Network Usage
+                  </Typography>
+                  <img src={altix[2]} alt="Ganglia chart" className={classes.imgResponsive} />
+                </GridListTile>
+              </GridList>
+              <GridList
+                cellHeight="auto"
+                cols={2}
+              >
+                <GridListTile style={{ textAlign: 'center' }}>
+                  <Typography
+                    variant="h6"
+                    className={classes.plotTitle}
+                  >
+                    <strong>[ICEx]</strong>
+                    {' '}
+                  Aggregated Cluster Load
+                  </Typography>
+                  <img src={icex[3]} alt="Ganglia chart" className={classes.imgResponsive} />
+                </GridListTile>
+                <GridListTile style={{ textAlign: 'center' }}>
+                  <Typography
+                    variant="h6"
+                    className={classes.plotTitle}
+                  >
+                    <strong>[Altix]</strong>
+                    {' '}
+                  Aggregated Cluster Load
+                  </Typography>
+                  <img src={altix[3]} alt="Ganglia chart" className={classes.imgResponsive} />
+                </GridListTile>
+              </GridList>
+            </div>
+          </Grid>
+        </Grid>
       </div>
+
     </React.Fragment>
   );
 }
