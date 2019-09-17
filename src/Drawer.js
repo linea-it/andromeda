e6e54cb36d12c61ef64d05f36fd8338a0e0bdb4d
Matheus Freitas <mtsallein@gmail.com>
2019-09-17 12:56:34 -0300

    Fixing nodes bug (#40)

--- src/Drawer.js
+++ src/Drawer.js
@@ -2,8 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-// import Toolbar from '@material-ui/core/Toolbar';
+// import AppBar from '@material-ui/core/AppBar';
 import List from '@material-ui/core/List';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Divider from '@material-ui/core/Divider';
@@ -19,8 +18,6 @@ import ListItemText from '@material-ui/core/ListItemText';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import Icon from '@material-ui/core/Icon';
 import Logo from './assets/img/linea.png';
-// import Report from './components/Report';
-// import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Footer from './components/Footer';
 import Icex from './components/Icex';
@@ -34,25 +31,25 @@ const useStyles = makeStyles(theme => ({
     display: 'flex',
     height: '100%',
   },
-  appBar: {
-    backgroundColor: '#fff',
-    boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2)',
-    color: '#000',
-    width: `calc(100% - ${theme.spacing(7) + 1}px)`,
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
+  // appBar: {
+  //   backgroundColor: '#fff',
+  //   boxShadow: '0px 2px 4px -1px rgba(0,0,0,0.2)',
+  //   color: '#000',
+  //   width: `calc(100% - ${theme.spacing(7) + 1}px)`,
+  //   zIndex: theme.zIndex.drawer + 1,
+  //   transition: theme.transitions.create(['width', 'margin'], {
+  //     easing: theme.transitions.easing.sharp,
+  //     duration: theme.transitions.duration.leavingScreen,
+  //   }),
+  // },
+  // appBarShift: {
+  //   marginLeft: drawerWidth,
+  //   width: `calc(100% - ${drawerWidth}px)`,
+  //   transition: theme.transitions.create(['width', 'margin'], {
+  //     easing: theme.transitions.easing.sharp,
+  //     duration: theme.transitions.duration.enteringScreen,
+  //   }),
+  // },
   menuButton: {
     marginRight: 36,
     color: '#fff',
@@ -193,16 +190,16 @@ function MiniDrawer() {
     <div className={classes.root}>
       <Router>
         <CssBaseline />
-        <AppBar
+        {/* <AppBar
           position="fixed"
           className={clsx(classes.appBar, {
             [classes.appBarShift]: open,
           })}
-        >
-          {/* <Toolbar className={classes.toolbar}>
+        > */}
+        {/* <Toolbar className={classes.toolbar}>
             <Login />
           </Toolbar> */}
-        </AppBar>
+        {/* </AppBar> */}
         <Drawer
           variant="permanent"
           className={clsx(classes.drawer, {
--- src/components/TableNode.js
+++ src/components/TableNode.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import {
   SearchState,
-  PagingState,
-  IntegratedPaging,
   SortingState,
   IntegratedSorting,
   IntegratedFiltering,
@@ -11,10 +9,9 @@ import {
 } from '@devexpress/dx-react-grid';
 import {
   Grid,
-  Table,
+  VirtualTable,
   Toolbar,
   SearchPanel,
-  PagingPanel,
   TableColumnVisibility,
 } from '@devexpress/dx-react-grid-material-ui';
 import Card from '@material-ui/core/Card';
@@ -26,10 +23,10 @@ import Dialog from '@material-ui/core/Dialog';
 import PropTypes from 'prop-types';
 import IconButton from '@material-ui/core/IconButton';
 import CloseIcon from '@material-ui/icons/Close';
-import CustomColumnChooser from './CustomColumnChooser';
-import CustomTableHeaderRowCell from './CustomTableHeaderRowCell';
-import { getNodes } from '../../api/Api';
-import CustomTableGroupRow from './CustomTableGroupRow';
+import CustomColumnChooser from '../utils/CustomColumnChooser';
+import CustomTableHeaderRowCell from '../utils/CustomTableHeaderRowCell';
+import { getNodes } from '../api/Api';
+import CustomTableGroupRow from '../utils/CustomTableGroupRow';
 
 
 const useStyles = makeStyles(theme => ({
@@ -66,6 +63,7 @@ function TableNode(props) {
   } = props;
 
   const [nodes, setNodes] = useState([]);
+  const [rows, setRows] = useState([]);
 
   useEffect(() => {
     getNodes()
@@ -86,24 +84,27 @@ function TableNode(props) {
     return r;
   }
 
-  const data = {
-    columns: [
-      { name: 'node', title: 'Node' },
-      { name: 'slot', title: 'Slot' },
-    ],
-    rows: jobs.length > 0 ? jobs.map((job) => {
+  const columns = [
+    { name: 'node', title: 'Node' },
+    { name: 'slot', title: 'Slot' },
+  ];
+
+  useEffect(() => {
+    let lines = [];
+    jobs.map((job) => {
       const slots = job.RemoteHost.split('@')[0];
       if (job.RequiresWholeMachine === 'True') {
         const slotsAmount = Number(nodes.filter(node => node.Name === job.RemoteHost)[0].TotalCpus.split('.')[0]);
-        return slotsIncrementor(slotsAmount, job.RemoteHost);
+        lines = lines.concat(slotsIncrementor(slotsAmount, job.RemoteHost));
+      } else {
+        lines = lines.concat(rows.concat([{
+          node: job.RemoteHost ? job.RemoteHost.split('.')[0].split('@')[1] : null,
+          slot: job.RemoteHost ? slots : null,
+        }]));
       }
-
-      return [{
-        node: job.RemoteHost ? job.RemoteHost.split('.')[0].split('@')[1] : null,
-        slot: job.RemoteHost ? slots : null,
-      }];
-    })[0] : [],
-  };
+    });
+    setRows(lines);
+  }, [jobs]);
 
 
   return (
@@ -127,33 +128,27 @@ function TableNode(props) {
         />
         <CardContent>
           <Grid
-            rows={data.rows}
-            columns={data.columns}
+            rows={rows}
+            columns={columns}
           >
             <SearchState />
-            <PagingState
-              defaultCurrentPage={0}
-              pageSize={10}
-            />
             <SortingState
               defaultSorting={[{ columnName: 'node', direction: 'asc' }]}
             />
             <GroupingState
               grouping={[{ columnName: 'node' }]}
             />
-            <IntegratedPaging />
             <IntegratedSorting />
             <IntegratedFiltering />
             <IntegratedGrouping />
-            <Table columnExtensions={data.tableColumnExtensions} />
+            <VirtualTable />
             <CustomTableHeaderRowCell />
             <CustomTableGroupRow
-              rows={data.rows}
+              rows={rows}
             />
             <TableColumnVisibility />
             <Toolbar />
             <SearchPanel />
-            <PagingPanel />
             <CustomColumnChooser />
           </Grid>
         </CardContent>
