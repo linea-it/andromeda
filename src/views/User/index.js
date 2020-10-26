4e5116abc98f8f2206d63949c0d62e08061b285f
Matheus Freitas <mtsallein@gmail.com>
2020-10-26 15:39:28 -0300

    Update index.js

--- src/views/User/index.js
+++ src/views/User/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useEffect, useState } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
